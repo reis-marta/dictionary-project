eacccd8e1de7f821bd8a1ff4ba534480c6871837
reis-marta <163763026+reis-marta@users.noreply.github.com>
2024-09-29 18:21:41 +0100

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,7 +6,7 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
-    alert(Searching);
+    alert(`Searching for ${keyword} definition`);
   }
 
   function handleKeywordChange(event) {
