31d9cd3852b9ca23848aea7204c979a281bee77f
reis-marta <163763026+reis-marta@users.noreply.github.com>
2024-10-26 17:17:32 +0100

    some Photos corrections

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -25,7 +25,7 @@ export default function Dictionary(props) {
     let pexelsApiKey =
       "Fvy81b4dOrfGFZAUJeCm6JnYbTQSSusoJ3wipYCyJQXG9na6KQwm4QEQ";
     let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
-    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    let headers = { Authorization: `${pexelsApiKey}` };
     axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
--- src/Photos.js
+++ src/Photos.js
@@ -8,10 +8,9 @@ export default function Photos(props) {
         <div className="row">
           {props.photos.map(function (photo, index) {
             return (
-              <div className="col-3" key={index}>
+              <div className="col-4" key={index}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
-                  <img src={photo.src.landscape} alt="" className="imh-fluid" />
-                  ;
+                  <img src={photo.src.landscape} alt="" className="img-fluid" />
                 </a>
               </div>
             );
